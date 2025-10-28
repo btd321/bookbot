ed4245789097d7207049d11e65d2261e21fc1cf8
btd321 <btd321@gmail.com>
2025-10-27 23:04:12 -0400

    added book_splitter and get_book_text

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+# Get book function
+
+def get_book_text(path_to_file):
+    with open(path_to_file) as f:
+        file_contents = f.read()
+        return file_contents
+
+def book_splitter(book_text):
+    book_words = book_text.split() 
+    num_words = len(book_words)
+    print (f"Found {num_words} total words")
+
+def main():
+    book_splitter(get_book_text("books/frankenstein.txt"))
+
+main()
